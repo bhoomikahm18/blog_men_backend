2b99c9c1882149b6151acfcb420ac79b3954ac4e
Bhoomika <bhoomikahm18@gmail.com>
2023-09-30 15:01:25 +0530

    Adding update blog function

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -1,6 +1,6 @@
 // import mongoose from "mongoose";
 const Blog = require("../model/Blog");
-// import User from "../model/User";
+const User = require("../model/User");
 
 const getAllBlogs = async (req, res, next) => {
     let blogs;
@@ -16,56 +16,60 @@ const getAllBlogs = async (req, res, next) => {
 };
 module.exports = getAllBlogs;
 
-// export const addBlog = async (req, res, next) => {
-//     const { title, description, image, user } = req.body;
+const addBlog = async (req, res, next) => {
+    const { title, description, image, user } = req.body;
 
-//     let existingUser;
-//     try {
-//         existingUser = await User.findById(user);
-//     } catch (err) {
-//         return console.log(err);
-//     }
-//     if (!existingUser) {
-//         return res.status(400).json({ message: "Unable TO FInd User By This ID" });
-//     }
-//     const blog = new Blog({
-//         title,
-//         description,
-//         image,
-//         user,
-//     });
-//     try {
-//         const session = await mongoose.startSession();
-//         session.startTransaction();
-//         await blog.save({ session });
-//         existingUser.blogs.push(blog);
-//         await existingUser.save({ session });
-//         await session.commitTransaction();
-//     } catch (err) {
-//         console.log(err);
-//         return res.status(500).json({ message: err });
-//     }
+    // let existingUser;
+    // try {
+    //     existingUser = await User.findById(user);
+    // } catch (err) {
+    //     return console.log(err);
+    // }
+    // if (!existingUser) {
+    //     return res.status(400).json({ message: "Unable TO FInd User By This ID" });
+    // }
+    const blog = new Blog({
+        title,
+        description,
+        image,
+        user,
+    });
+    try {
+        // const session = await mongoose.startSession();
+        // session.startTransaction();
+        await blog.save();
+        // existingUser.blogs.push(blog);
+        // await existingUser.save({ session });
+        // await session.commitTransaction();
+    } catch (err) {
+        console.log(err);
+        // return res.status(500).json({ message: err });
+    }
 
-//     return res.status(200).json({ blog });
-// };
+    return res.status(200).json({ blog });
+};
 
-// export const updateBlog = async (req, res, next) => {
-//     const { title, description } = req.body;
-//     const blogId = req.params.id;
-//     let blog;
-//     try {
-//         blog = await Blog.findByIdAndUpdate(blogId, {
-//             title,
-//             description,
-//         });
-//     } catch (err) {
-//         return console.log(err);
-//     }
-//     if (!blog) {
-//         return res.status(500).json({ message: "Unable To Update The Blog" });
-//     }
-//     return res.status(200).json({ blog });
-// };
+module.exports = addBlog;
+
+const updateBlog = async (req, res, next) => {
+    const { title, description } = req.body;
+    const blogId = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findByIdAndUpdate(blogId, {
+            title,
+            description,
+        });
+    } catch (err) {
+        return console.log(err);
+    }
+    if (!blog) {
+        return res.status(500).json({ message: "Unable To Update The Blog" });
+    }
+    return res.status(200).json({ blog });
+};
+
+module.exports = updateBlog;
 
 // export const getById = async (req, res, next) => {
 //     const id = req.params.id;
--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const getAllBlogs = require('../controllers/blog-controller.js');
 const addBlog = require('../controllers/blog-controller.js');
+const userBlogs = require('../controllers/blog-controller.js');
 const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
 blogRouter.post("/add", addBlog);
-// blogRouter.put("/update/:id", updateBlog);
+blogRouter.put("/update/:id", updateBlog);
 // blogRouter.get("/:id", getById);
 // blogRouter.delete("/:id", deleteBlog);
 // blogRouter.get("/user/:id", getByUserId);
