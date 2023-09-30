ef4d58d1205a7518cc9511a345f5e3249801d258
Bhoomika <bhoomikahm18@gmail.com>
2023-09-30 12:20:41 +0530

    Creating users

--- model/User.js
+++ model/User.js
@@ -17,7 +17,6 @@ const userSchema = new Schema({
     required: true,
     minlength: 6,
   },
-//   blogs: [{ type: mongoose.Types.ObjectId, ref: "Blog", required: true }],
 });
 
 module.exports = mongoose.model("User", userSchema);
