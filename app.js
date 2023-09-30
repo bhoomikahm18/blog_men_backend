abff83d3fdf0bc747dbd306dc7e1ed4fd2748cd5
Bhoomika <bhoomikahm18@gmail.com>
2023-09-30 12:18:00 +0530

    Calling users

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const userRouter = require('./routes/user-routes.js');
 
 const app = express();
 
-app.use(express.json());
-app.use("/api/user", router);
+// app.use(express.json());
+app.use("/api/user", userRouter);
 
 mongoose.connect("mongodb+srv://bhoomikahm18:bhoomi123@cluster0.5qlvwpp.mongodb.net/Blog?retryWrites=true&w=majority")
     .then(() => app.listen(5000))
---
+++ controllers/user-controller.js
@@ -0,0 +1,46 @@
+const User = require("../model/User.js");
+// const bcrypt from "bcryptjs";
+
+const getAllUser = async (req, res, next) => {
+    let users;
+    try {
+        users = await User.find();
+    } catch (err) {
+        console.log(err);
+    }
+    if (!users) {
+        return res.status(404).json({ message: "No Users Found" });
+    }
+    return res.status(200).json({ users });
+};
+module.exports = getAllUser;
+
+// const signup = async (req, res, next) => {
+//     const { name, email, password } = req.body;
+//     let existingUser;
+//     try {
+//         existingUser = await User.findOne({ email });
+//     } catch (err) {
+//         return console.log(err);
+//     }
+//     if (existingUser) {
+//         return res
+//             .status(400)
+//             .json({ message: "User Already Exists! Login Instead" });
+//     }
+//     const hashedPassword = bcrypt.hashSync(password);
+
+//     const user = new User({
+//         name,
+//         email,
+//         password: hashedPassword,
+//         blogs: [],
+//     });
+
+//     try {
+//         await user.save();
+//     } catch (err) {
+//         return console.log(err);
+//     }
+//     return res.status(201).json({ user });
+// };
