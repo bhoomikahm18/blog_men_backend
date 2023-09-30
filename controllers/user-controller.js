d5e053cedd7fe70a089a490b0e184f1593de4ecd
Bhoomika <bhoomikahm18@gmail.com>
2023-09-30 12:58:11 +0530

    Adding login fuction

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -49,3 +49,26 @@ const signup = async (req, res, next) => {
 };
 
 module.exports = signup;
+
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email });
+    } catch (err) {
+        return console.log(err);
+    }
+    if (!existingUser) {
+        return res.status(404).json({ message: "Couldnt Find User By This Email" });
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+    if (!isPasswordCorrect) {
+        return res.status(400).json({ message: "Incorrect Password" });
+    }
+    return res
+        .status(200)
+        .json({ message: "Login Successfull", user: existingUser });
+};
+
+module.exports = login;
